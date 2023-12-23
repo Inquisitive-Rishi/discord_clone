1bcadac2d372468733c7cd4710a85ad2c39c6e8e
Rishi Raj <rishiraj007bgp@gmail.com>
2023-12-23 18:48:19 +0530

    add config.js file to manage enviroment variable

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const { info, error } = require('./utils/logger')
+const config = require('./utils/config')
 const cors = require('cors')
 const http = require('http')
 const mongoose = require('mongoose')
 
 const authRouter = require('./routers/authRoutes')
+
+const url = config.MONGODB_URI;
+const PORT = config.PORT;
 
 const app = express()
 const server = http.createServer(app)
@@ -13,15 +17,14 @@ const server = http.createServer(app)
 app.use(cors())
 app.use(express.json())
 
-info('connecting to DB', process.env.MONGODB_URI)
+info('connecting to DB', url)
 
 app.use('/api/auth', authRouter)
 
-const PORT = process.env.PORT;
 server.listen(PORT, () => {
     info('Server is listening on port', PORT);
 })
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(url)
 .then(() => info('Connected to MongoDB'))
 .catch(err => error('Error connecting', err))
