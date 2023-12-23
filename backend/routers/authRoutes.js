31a60605ae17b0d10384fe283b09939ea687b4ef
Rishi Raj <rishiraj007bgp@gmail.com>
2023-12-23 19:11:31 +0530

    add validators for incoming request

--- backend/routers/authRoutes.js
+++ backend/routers/authRoutes.js
@@ -1,10 +1,26 @@
 const router = require('express').Router()
+const Joi = require('joi')
+const validator = require('express-joi-validation').createValidator({})
 
-router.post('/register', (req,res) => {
+const registerSchema = Joi.object({
+    username: Joi.string().min(6).max(14),
+    password: Joi.string().min(8).max(14),
+    email: Joi.string().email()
+})
+
+const loginSchema = Joi.object({
+    username: Joi.string().min(6).max(14),
+    password: Joi.string().min(8).max(14)
+})
+
+const registerValidation = validator.body(registerSchema)
+const loginValidation = validator.body(loginSchema)
+
+router.post('/register', registerValidation, async (req,res) => {
     res.send('This is register page')
 })
 
-router.post('/login', (req,res) => {
+router.post('/login', loginValidation, async (req,res) => {
     res.send('This is login page')
 })
 
