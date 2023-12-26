9e5630581bff227b53156ea3afd1d96072376152
Rishi Raj <rishiraj007bgp@gmail.com>
2023-12-26 22:14:24 +0530

    add login functionality

--- backend/controllers/postLogin.js
+++ backend/controllers/postLogin.js
@@ -1,5 +1,28 @@
+const User = require('../models/user.model')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const postLogin = async (req,res) => {
-    res.send('This is login page')
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email: email })
+    
+        if (user && (await bcrypt.compare(password, user.password))) {
+            const token = 'JWT token'
+            
+            return res.status(200).json({
+                userDetails: {
+                    username: user.username,
+                    email: user.email,
+                    token: token
+                }
+            })
+        }
+        return res.status(400).send('Invalid credentials: try again!')    
+    } catch (error) {
+        return res.status(500).send('Something went wrong, please try again :(')
+    }
 }
 
 module.exports = postLogin;
